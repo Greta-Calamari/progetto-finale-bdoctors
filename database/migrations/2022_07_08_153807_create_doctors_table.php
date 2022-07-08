8f6cb3d71a85ef5c1405cf15ac2cd06ce0ff7b10
maquattrocchi <martinaquattrocchi5@gmail.com>
2022-07-08 18:41:24 +0200

    model + migration + array doctors

---
+++ database/migrations/2022_07_08_153807_create_doctors_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateDoctorsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('doctors', function (Blueprint $table) {
+            $table->id();
+            $table->string('name', 50);
+            $table->string('surname', 50);
+            $table->string('address');
+            $table->string('photo');
+            $table->string('curriculum_vitae');
+            $table->string('cell_number', 20);
+            $table->text('services');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('doctors');
+    }
+}
