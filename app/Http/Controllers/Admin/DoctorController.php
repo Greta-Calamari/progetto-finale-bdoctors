ae822a7a05f5b5f4f687b34a0061ef893111f789
Pavel-Stefano <pavelstefanominuti@gmail.com>
2022-07-09 12:25:13 +0200

    doctorsController

--- app/Http/Controllers/Admin/DoctorController.php
+++ app/Http/Controllers/Admin/DoctorController.php
@@ -4,10 +4,20 @@ namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use App\Doctor;
 
 class DoctorController extends Controller
 {
+    protected $validationRule = [
+        "name" => "required|string|max:50",
+        "surname" => "required|string|max:50",
+        "address" => "required|string",
+        "photo" => "required|mimes:jpeg,bmp,png,svg,jpg,webp|max:2048",
+        "curriculum_vitae" => "required|string|mimes:pdf,doc,odt,csv",
+        "cell_number" => "required|string|max:20",
+        "services" => "required|text",
+    ];
     /**
      * Display a listing of the resource.
      *
@@ -27,7 +37,7 @@ class DoctorController extends Controller
      */
     public function create()
     {
-        //
+        return view('admin.doctors.create');
     }
 
     /**
@@ -38,7 +48,21 @@ class DoctorController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $data = $request->all();
+
+        $newDoctor = new Doctor();
+        $newDoctor->name = $data['name'];
+        $newDoctor->surname = $data['surname'];
+        $newDoctor->address = $data['address'];
+        $newDoctor->photo = $data['photo'];
+        $newDoctor->curriculum_vitae = $data['curriculum_vitae'];
+        $newDoctor->cell_number = $data['cell_number'];
+        $newDoctor->services = $data['services'];
+
+        $newDoctor->save();
+
+        return redirect()->route('admin.doctors.show', $newDoctor->id);
+
     }
 
     /**
@@ -47,9 +71,14 @@ class DoctorController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function show($id)
+    public function show(Doctor $doctor)
     {
-        //
+        $currentUser = Auth::user();
+        if($currentUser->id != $doctor->user_id) {
+            abort(404);
+        }
+
+        return view('admin.doctors.show', compact('doctor'));
     }
 
     /**
@@ -58,9 +87,9 @@ class DoctorController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function edit($id)
+    public function edit(Doctor $doctor)
     {
-        //
+        return view('admin.doctors.edit', compact('doctor'));
     }
 
     /**
@@ -70,9 +99,13 @@ class DoctorController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, $id)
+    public function update(Request $request, Doctor $doctor)
     {
-        //
+        $data = $request->all();
+
+        $doctor->update();
+
+        return redirect()->route('admin.doctors.show', $doctor->id);
     }
 
     /**
@@ -81,8 +114,9 @@ class DoctorController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function destroy($id)
+    public function destroy(Doctor $doctor)
     {
-        //
+        $doctor->delete();
+        return redirect()->route('admin.doctors.index')->with("message", "doctor with id: {$doctor->id} successfully deleted !");
     }
 }
