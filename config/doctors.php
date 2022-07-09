e0329d365606ba1bb957be6ceb80a3ce78c84ea4
ladykakki <greta_calamari@hotmail.it>
2022-07-09 11:38:44 +0200

    modifica array

--- config/doctors.php
+++ config/doctors.php
@@ -2,6 +2,10 @@
 
 return [
     [
+        'email'=>'milomarziali@dottori.com',
+        'password'=>'doctors59',
+        'user_name' => 'MiloMarziali',
+        'user_id' => '1',
         'name' => 'Milo',
         'surname' => 'Marziali',
         'address' => 'Via Pompeo, 7',
@@ -11,6 +15,10 @@ return [
         'services' => "Scienze dell'Alimentazione, Medicina estetica, Terapia del dolore",
     ],
     [
+        'email'=>'cristianpellegrini@dottori.com',
+        'password'=>'doctors59',
+        'user_name' => 'CristianPellegrini',
+        'user_id' => '2',
         'name' => 'Cristian',
         'surname' => 'Pellegrini',
         'address' => 'Via Lodovico, 15',
@@ -20,6 +28,10 @@ return [
         'services' => 'Ecocolordoppler dei vasi periferici, Ecocolordoppler dei tronchi sovra aortici, Elettrocardiogramma holter',
     ],
     [
+        'email'=>'marcozampieri@dottori.com',
+        'password'=>'doctors59',
+        'user_name' => 'MarcoZampieri',
+        'user_id' => '3',
         'name' => 'Marco',
         'surname' => 'Zampieri',
         'address' => 'Via degli Oleandri, 11',
@@ -29,6 +41,10 @@ return [
         'services' => 'Coronarografia, Cataratta, Visita oculistica',
     ],
     [
+        'email'=>'vilmamaria@dottori.com',
+        'password'=>'doctors59',
+        'user_name' => 'VilmaMaria',
+        'user_id' => '4',
         'name' => 'Vilma Maria',
         'surname' => 'Crivellaro',
         'address' => 'Via Baggio, 20',
@@ -38,6 +54,10 @@ return [
         'services' => 'Radiografia ossea e delle articolazioni, Radiografia completa del tubo digerente, Radiografia completa del tubo digerente',
     ],
     [
+        'email'=>'giorgiofrancuzzi@dottori.com',
+        'password'=>'doctors59',
+        'user_name' => 'GiorgioFrancuzzi',
+        'user_id' => '5',
         'name' => 'Giorgio',
         'surname' => 'Francuzzi',
         'address' => 'Via Mar Nero, 30',
@@ -47,6 +67,10 @@ return [
         'services' => 'Riabilitazione motoria, Riabilitazione respiratoria, Logopedia',
     ],
     [
+        'email'=>'martinaseravelli@dottori.com',
+        'password'=>'doctors59',
+        'user_name' => 'MartinaSeravelli',
+        'user_id' => '6',
         'name' => 'Martina',
         'surname' => 'Seravelli',
         'address' => 'Viale Nazario Sauro, 5',
@@ -56,6 +80,10 @@ return [
         'services' => 'Visita neurologica, EEG, EMG, Potenziali Evocati',
     ],
     [
+        'email'=>'mariarosaromeo@dottori.com',
+        'password'=>'doctors59',
+        'user_name' => 'MartinaRomeo',
+        'user_id' => '7',
         'name' => 'Mariarosa',
         'surname' => 'Romeo',
         'address' => 'Via Ugo Bassi, 22',
@@ -65,6 +93,10 @@ return [
         'services' => 'Visita dermatologica, Test allergologici',
     ],
     [
+        'email'=>'sergiopiombino@dottori.com',
+        'password'=>'doctors59',
+        'user_name' => 'SergioPiombino',
+        'user_id' => '8',
         'name' => 'Sergio',
         'surname' => 'Piombino',
         'address' => 'Via Giulio Cesare Procaccini, 4',
@@ -74,6 +106,10 @@ return [
         'services' => 'Impedenzometria, Potenziali evocati, Test vestibolare',
     ],
     [
+        'email'=>'carlarando@dottori.com',
+        'password'=>'doctors59',
+        'user_name' => 'CarlaRando',
+        'user_id' => '9',
         'name' => 'Carla',
         'surname' => 'Rando',
         'address' => 'Via Monte Amiata, 9',
@@ -83,6 +119,10 @@ return [
         'services' => 'Colloquio psichiatrico, Elettroencefalogramma, Visita neurologica',
     ],
     [
+        'email'=>'chiarapirozzi@dottori.com',
+        'password'=>'doctors59',
+        'user_name' => 'ChiaraPirozzi',
+        'user_id' => '10',
         'name' => 'Chiara',
         'surname' => 'Pirozzi',
         'address' => 'Via Alfonso Lamarmora, 8',
