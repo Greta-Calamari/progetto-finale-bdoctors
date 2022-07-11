04bc1bf1d34271f38c15436fe59f8c93917d785a
dumy1994 <dumitru_budau@yahoo.it>
2022-07-11 10:33:44 +0200

    messages

--- app/Doctor.php
+++ app/Doctor.php
@@ -7,8 +7,11 @@ use Illuminate\Database\Eloquent\Model;
 class Doctor extends Model
 {
     protected $guarded = [];
-    
+
     public function user(){
         return $this->belongsTo('App\User');
+    }
+    public function messages(){
+        return $this->hasMany('App\Messages');
     }
 }
