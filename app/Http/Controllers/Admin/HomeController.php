087826f82544e5af4d9b144435353d167c86b6d1
dumy1994 <dumitru_budau@yahoo.it>
2022-07-15 10:22:58 +0200

    prova pivot

--- app/Http/Controllers/Admin/HomeController.php
+++ app/Http/Controllers/Admin/HomeController.php
@@ -1,6 +1,9 @@
 <?php
 
 namespace App\Http\Controllers\Admin;
+use App\Sponsor;
+use App\Doctor;
+
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
@@ -8,6 +11,9 @@ use Illuminate\Http\Request;
 class HomeController extends Controller
 {
     public function index(){
+        $doctor = Doctor::find(1);
+        $doctor->sponsors()->attach(1);
+        dd($doctor->sponsors);
         return view('admin.home');
     }
 }
--- app/Sponsor.php
+++ app/Sponsor.php
@@ -12,3 +12,46 @@ class Sponsor extends Model
         return $this->belongsToMany('App\Doctor');
     }
 }
+
+
+
+
+// <?php
+
+// use Illuminate\Database\Migrations\Migration;
+// use Illuminate\Database\Schema\Blueprint;
+// use Illuminate\Support\Facades\Schema;
+
+// class CreateSponsorDoctor extends Migration
+// {
+//     /**
+//      * Run the migrations.
+//      *
+//      * @return void
+//      */
+//     public function up()
+//     {
+//         Schema::create('doctor_sponsor', function (Blueprint $table) {
+//             $table->id();
+//             $table->foreignId('doctor_id')->constrained()->onDelete('cascade');
+//             $table->foreignId('sponsor_id')->constrained()->onDelete('cascade');
+//             $table->dateTime('date_start');
+//             $table->dateTime('date_end');
+//             $table->timestamps();
+//         });
+//     }
+
+//     /**
+//      * Reverse the migrations.
+//      *
+//      * @return void
+//      */
+//     public function down()
+//     {
+//         Schema::dropIfExists('sponsor_doctor');
+//     }
+// }
+
+
+
+
