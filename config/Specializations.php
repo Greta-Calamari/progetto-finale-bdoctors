365bf666789cc8e2363826c6a0c2d3c049fb0be9
dumy1994 <dumitru_budau@yahoo.it>
2022-07-18 10:36:25 +0200

    background

--- config/Specializations.php
+++
@@ -1,3 +0,0 @@
-<?php
-return ['Cardiochirurgia', 'Medicina interna', 'Medicina d’emergenza-urgenza', 'Anestesia, rianimazione e terapia intensiva e del dolore', 'Audiologia e foniatria','Medicina fisica e riabilitativa', 'Farmacologia e tossicologia clinica', 'Genetica medica', 'Igiene e medicina preventiva', 'Medicina del lavoro', 'Medicina legale', 'Statistica sanitaria e biometria', 'Chirurgia generale', 'Chirurgia pediatrica', 'Chirurgia plastica, ricostruttiva ed estetica', 'Ginecologia ed ostetricia', 'Ortopedia e traumatologia','Urologia'];
-?>
--- resources/js/pages/DoctorComponent.vue
+++ resources/js/pages/DoctorComponent.vue
@@ -278,7 +278,9 @@ export default {
 <style lang="scss">
 @import '../../sass/variables';
 section{
-    background-color: $general-light-blue;
+     background: linear-gradient(0deg, hsla(251, 42%, 50%, 1) 40%, hsla(231, 51%, 60%, 1) 100%);
+
+    background: -moz-linear-gradient(0deg, hsla(251, 42%, 50%, 1) 40%, hsla(231, 51%, 60%, 1) 100%);
     width: 100%;
     min-height: 60vh;
     // doctor 
