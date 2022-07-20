148a3f21a784c0a50159d1645fedfad567f9a853
ladykakki <greta_calamari@hotmail.it>
2022-07-20 09:54:55 +0200

    icona e titolo sito

--- resources/views/guest/home.blade.php
+++ resources/views/guest/home.blade.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="{{asset('css/front.css')}}">
-    <title>Laravel Boolpress</title>
+    <title>Dottori</title>
 </head>
 <body>
     <div id="root">
