216f5ed2a7780ccaf3bb0d880d7895e18bef6548
Giulia-92 <CIONCO.GIULIA@GMAIL.COM>
2022-07-21 12:13:19 +0200

    modifiche show

--- resources/views/admin/doctors/show.blade.php
+++ resources/views/admin/doctors/show.blade.php
@@ -4,16 +4,28 @@
 @section('content')
 
     <section>
-        <div style="margin-left:10%">
-            
-            <img src="{{asset('storage/' . $doctor->photo)}}" class="rounded" style="border-radius:5%" alt="{{$doctor->surname}}">
-        </div>
-        <div class="col-sm-4" style="margin-left: 20%">
-    <div class="card" style="width: 69rem">
-        <div class="text-center">
-            
-        </div>
-        <div class="card-body">
+        <div class="container modfica-profile">
+    
+    <form action="{{route('admin.doctors.update', $doctor->id)}}" method="post" enctype="multipart/form-data">
+        @csrf
+        @method('PUT')
+    
+        <div class="row">
+            <div class="col col-md-6 col-lg-3 ">
+                <div class="mb-3 mt-3">
+                    
+                    <div class="container-photo">
+                        <img id="imgPreview" src="{{asset('storage/' . $doctor->photo)}}">
+                    </div>
+                </div>
+            </div>
+
+            <div class="col col-sm-12 col-md-6 col-lg-5">
+                <h2>Modifica il tuo profilo</h2>
+                <div class="row">
+                    <div class="col-6">
+                        <div class="mb-3">
+                                <div class="card-body">
             <div class="card">
                 <div class="card-body">
                     {{$doctor->name}} {{$doctor->surname}} 
@@ -45,7 +57,18 @@
                             <li>{{$specialization->name}}</li>
                         @endforeach
                     </ul>
-                    <ul>
+                        </div>
+                    </div>
+                    <div class="col-6">
+                        
+                    </div>
+                </div>
+
+            </div>
+            
+            <div class="col col-sm-12 col-md-12 col-lg-4">
+                <!--<div class="mb-3 mt-3">
+                     <ul>
                         <h3>Review</h3>
                         @foreach ($doctor->reviews as $review)
                             <li>Nome: {{$review->name}}</li>
@@ -61,61 +84,35 @@
                             <li>Content: {{$message->content}}</li>
                         @endforeach 
                     </ul>  
-               
+                </div>-->
+            </div>
+
+             <a href="{{route('admin.doctors.edit', $doctor->id)}}" class="btn cs_btn text-center">Modifica Profilo</a>
+        </div>
+
        
-       
-        <a href="{{route('admin.doctors.edit', $doctor->id)}}" class="btn mybtn" style="margin-left: 45%">Modifica Profilo</a>
-      </div>
-    </div>
-  </div>
+        
+        <div class="note">
+             <div class="mb-3 mt-3">
+                     <ul>
+                        <h3>Review</h3>
+                        @foreach ($doctor->reviews as $review)
+                            <li>Nome: {{$review->name}}</li>
+                            <li>Commento: {{$review->comment}}</li>
+                            <li>Voto: {{$review->votes}}</li>
+                        @endforeach
+                    </ul>  
+                    <ul>
+                        <h3>Messages</h3>
+                        @foreach ($doctor->messages as $message)
+                            <li>Nome: {{$message->name}}</li>
+                            <li>Email: {{$message->email}}</li>
+                            <li>Content: {{$message->content}}</li>
+                        @endforeach 
+                    </ul>  
+                </div>
+        </div>
+    </form>
+</div>
 
 @endsection
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-        <!--<h1>{{$doctor->name}} {{$doctor->surname}} </h1>
-        <a href="{{route('admin.doctors.edit', $doctor->id)}}">EDit</a>
-
-        <p>{{$doctor->address}} </p>
-        <p>{{$doctor->cell_number}} </p>
-        <img src="{{asset('storage/' . $doctor->photo)}}" alt="{{$doctor->surname}}">
-        <iframe src="{{asset('storage/' . $doctor->curriculum_vitae)}}" width=”300” height=”800”></iframe>
-            
-        
-        <p>{{$doctor->services}}</p>
-        <ul>
-            @foreach ($doctor->specializations as $specialization)
-                <li>{{$specialization->name}}</li>
-            @endforeach
-        </ul>
-
-        <ul>
-            <h3>Review</h3>
-            @foreach ($doctor->reviews as $review)
-                <li>Nome: {{$review->name}}</li>
-                <li>Commento: {{$review->comment}}</li>
-                <li>Voto: {{$review->votes}}</li>
-            @endforeach
-        </ul>
-
-        <ul>
-            <h3>Messages</h3>
-            @foreach ($doctor->messages as $message)
-                <li>Nome: {{$message->name}}</li>
-                <li>Email: {{$message->email}}</li>
-                <li>Content: {{$message->content}}</li>
-            @endforeach
-        </ul>
-        
-    </section>-->
