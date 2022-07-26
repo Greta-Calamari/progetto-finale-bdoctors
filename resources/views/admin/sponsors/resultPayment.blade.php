b510ec2fba9bc25fbc447c5b007ec5eb91657190
maquattrocchi <martinaquattrocchi5@gmail.com>
2022-07-26 14:51:46 +0200

    fine grafica pagina di pagamdento

--- resources/views/admin/sponsors/resultPayment.blade.php
+++ resources/views/admin/sponsors/resultPayment.blade.php
@@ -4,28 +4,32 @@
     {{-- @dd($risultato) --}}
     @if($risultato['result'] == true)
         <div id="paymentResult" class="container">
-            <h1>Ti ringraziamo per il tuo acquisto!</h1>
-            <h4>La tua sponsorizzazione {{$sponsorization->name}} è stata correttamente attivata.</h4>
-            <div class="box-info">
-                <p class="margin">
-                    <span class="label"> Inizio sponsorizzazione</span>
-                    <span class="result"> {{date("d-m-Y, h:i:s", strtotime($dateStart))}}</span>
-                </p> 
-                <p class="margin">
-                    <span class="label"> Fine sponsorizzazione</span>
-                    <span class="result">{{date("d-m-Y, h:i:s", strtotime($dateEnd))}}</span>
-                </p> 
-            </div>
-            <div class="d-flex justify-content-between">
-                <a class="cs_link" href="{{url('/') }}"><i class="fa-solid fa-house"></i>Torna alla home</a>
-                <a class="cs_link" href="{{route('admin.home')}}">Torna alla dashboard<i class="fa-solid fa-user"></i></a>
+            <div class="box">
+                <h1>Ti ringraziamo per il tuo acquisto!</h1>
+                <h4>La tua sponsorizzazione {{$sponsorization->name}} è stata correttamente attivata.</h4>
+                <div class="box-info">
+                    <p class="border">
+                        <span class="label"> Inizio sponsorizzazione</span>
+                        <span class="result"> {{date("d-m-Y, h:i:s", strtotime($dateStart))}}</span>
+                    </p> 
+                    <p>
+                        <span class="label"> Fine sponsorizzazione</span>
+                        <span class="result">{{date("d-m-Y, h:i:s", strtotime($dateEnd))}}</span>
+                    </p> 
+                </div>
+                <div class="d-flex justify-content-between">
+                    <a class="cs_link" href="{{url('/') }}"><i class="fa-solid fa-house"></i>Torna alla home</a>
+                    <a class="cs_link" href="{{route('admin.home')}}">Torna alla dashboard<i class="fa-solid fa-user"></i></a>
+                </div>
             </div>
         </div>
     @else
     <div id="paymentResult" class="container">
-        <h1>Ci dispiace!</h1>
-        <h4>Purtroppo il pagamento non è andato a buon fine. Ti consigliamo di riprovare.</h4>
-        <a class="cs_link text-center d-block" href="{{route('admin.home')}}">Torna alla dashboard<i class="fa-solid fa-user"></i></a>
+        <div class="box">
+            <h1>Ci dispiace!</h1>
+            <h4>Purtroppo il pagamento non è andato a buon fine. Ti consigliamo di riprovare.</h4>
+            <a class="cs_link text-center d-block margin" href="{{route('admin.home')}}">Torna alla dashboard<i class="fa-solid fa-user"></i></a>
+        </div>
     </div>
     @endif
     
